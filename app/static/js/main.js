18a87e2ed72d4c412fd777c442e2c3ec41cc48fc
sahirjaggi <sjaggi95@gmail.com>
2015-05-03 16:44:33 -0400

    Add, View, and Edit Contact pages all turned into modal, although some error persists

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,19 @@
 function deleteContact(cid) {
-    if (window.confirm("<strong>Portfolio deletion</strong>You can't undo this action. Are you sure?")) {
+    if (window.confirm("Are you sure you want to delete this contact?")) {
         window.location.href = "/delete/" + cid;
     }
+}
+
+function contactOverlay(cid) {
+    var loc = "/list_item/" + cid;
+    var del = "javascript:deleteContact('" + cid + "');"
+    document.getElementById('contact_frame').setAttribute('src', loc);
+    document.getElementById('delete').setAttribute('href', del);
+	el = document.getElementById("contact_overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
+function newOverlay() {
+	el = document.getElementById("new_overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
